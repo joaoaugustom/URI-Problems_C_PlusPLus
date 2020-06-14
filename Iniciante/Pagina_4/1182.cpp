0aff8d0ffab68694973688a411dd21b0b316603a
joaoaugustom <joaoaugusto.goncalves@hotmail.com>
2020-06-14 17:20:24 -0300

    Update 1182.1

---
+++ Iniciante/Pagina_4/1182.cpp
@@ -0,0 +1,37 @@
+// URI Online Judge - 1182
+// Coluna na Matriz
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    double C[12][12], aux = 0;
+    int L = 0;
+    char T;
+
+    cin >> L;
+    cin >> T;
+
+    for (int i = 0; i < 12; i++) {
+        for (int j = 0; j < 12; j++) {
+            cin >> C[i][j];
+        }
+    }
+
+    for (int i = 0; i < 12; i++) {
+        aux += C[i][L];
+    }
+    if (T == 'S') {
+        cout << fixed << setprecision(1) << aux << endl;
+    }
+    
+    if (T == 'M') {
+        aux /= 12;
+
+        cout << fixed << setprecision(1) << aux << endl;
+    }
+
+    return 0;
+}
